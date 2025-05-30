be9057193e96062f09bd7b3b48f89278ea67d36f
Cameron B <cameronenginebytes@gmail.com>
2025-05-30 11:50:48 -0600

    added instanced rendering.

--- astraltrail/__main__.py
+++ astraltrail/__main__.py
@@ -5,18 +5,19 @@ import numpy as np
 import ctypes as ct
 
 triangle_mesh = np.array([
-    [-0.5, 0.5, 0.0],
-    [-0.5, -0.5, 0.0],
-    [0.5, -0.5, 0.0],
+    [-0.05, 0.05, 0.0],
+    [-0.05, -0.05, 0.0],
+    [0.05, -0.05, 0.0],
 ], np.float32)
 
 vertex_shader_source_string = b'''
 #version 330 core
 
 layout(location = 0) in vec3 aPos;
+layout(location = 1) in mat4 model;
 
 void main() {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = model * vec4(aPos, 1.0);
 }
 '''
 
@@ -33,7 +34,7 @@ void main() {
 def main():
     window = initiate_window()
 
-    models = configure_scene()
+    models, model_count = configure_scene()
 
     configure_gl()
 
@@ -44,7 +45,7 @@ def main():
     def on_draw():
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
         gl.glUseProgram(render_program)
-        gl.glDrawArraysInstanced(gl.GL_TRIANGLES, 0, 3, 1)
+        gl.glDrawArraysInstanced(gl.GL_TRIANGLES, 0, 3, model_count)
     
     window.push_handlers(on_draw)
 
@@ -58,11 +59,14 @@ def setup_render_buffers(models):
 
     vao_triangle = gl.GLuint()
     vbo_triangle = gl.GLuint()
+    vbo_triangle_instances = gl.GLuint()
 
     gl.glGenVertexArrays(1, ct.byref(vao_triangle))
     gl.glGenBuffers(1, ct.byref(vbo_triangle))
+    gl.glGenBuffers(1, ct.byref(vbo_triangle_instances))
 
     gl.glBindVertexArray(vao_triangle)
+
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo_triangle)
     gl.glBufferData(
         gl.GL_ARRAY_BUFFER,
@@ -78,10 +82,33 @@ def setup_render_buffers(models):
         0,
         ct.c_void_p(0)
     )
+    
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo_triangle_instances)
+    gl.glBufferData(
+        gl.GL_ARRAY_BUFFER, 
+        models['test-triangles'].nbytes,
+        models['test-triangles'].ctypes.data_as(ct.POINTER(ct.c_float)),
+        gl.GL_STATIC_DRAW
+    )
+    for i in range(4):
+        loc = 1 + i
+        gl.glEnableVertexAttribArray(loc)
+        gl.glVertexAttribPointer(
+            loc,
+            4,
+            gl.GL_FLOAT,
+            gl.GL_FALSE, 
+            64,
+            ct.c_void_p(i * 16)
+        )
+        gl.glVertexAttribDivisor(loc, 1)
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
     gl.glEnableVertexAttribArray(0)
 
     buffers['vao-triangle'] = vao_triangle
     buffers['vbo-triangle'] = vbo_triangle
+    buffers['vbo_triangle_instances'] = vbo_triangle_instances
 
     return buffers
 
@@ -127,7 +154,9 @@ def configure_scene():
 
     grid_size = 4
 
-    sep = 2
+    sep = 0.2
+
+    model_count = 0
 
     for i in range(grid_size):
         for j in range(grid_size):
@@ -137,17 +166,19 @@ def configure_scene():
             model[0, 3] = posx
             model[1, 3] = posy
 
-            models['test-triangles'].append(model.T)
+            models['test-triangles'].append(model.T.flatten())
+
+            model_count += 1
 
     models['test-triangles'] = np.array(models['test-triangles'], dtype=np.float32)
 
-    return models
+    return models, model_count
 
 def initiate_window():
 
-    width = 800
-    height = 600
-    caption = 'triangle'
+    width = 1280
+    height = 720
+    caption = 'triangles'
 
     return pyglet.window.Window(width, height, caption)
 
