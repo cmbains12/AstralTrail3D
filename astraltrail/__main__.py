1506cf872eedb6d2b07d9db8a7f369bc7007fb1e
Cameron B <cameronenginebytes@gmail.com>
2025-05-30 12:24:32 -0600

    drawing from two separate vertex array objects in each from: squares and triangles

--- astraltrail/__main__.py
+++ astraltrail/__main__.py
@@ -8,6 +8,11 @@ width = 1280
 height = 720
 caption = 'squares'
 
+triangle_mesh = np.array([
+    [-0.05, 0.05, 0.0],
+    [-0.05, -0.05, 0.0],
+    [0.05, -0.05, 0.0]
+], dtype=np.float32)
 
 square_vertices = np.array([
     [-0.05, 0.05, 0.0],
@@ -47,7 +52,7 @@ void main() {
 def main():
     window = initiate_window()
 
-    models, model_count = configure_scene()
+    models, square_count, triangle_count = configure_scene()
 
     configure_gl()
 
@@ -58,8 +63,11 @@ def main():
     def on_draw():
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
         gl.glUseProgram(render_program)
-        gl.glDrawArraysInstanced(gl.GL_TRIANGLES, 0, 6, model_count)
-    
+        gl.glBindVertexArray(buffers['vao-square'])
+        gl.glDrawArraysInstanced(gl.GL_TRIANGLES, 0, 6, square_count)
+        gl.glBindVertexArray(buffers['vao-triangle'])
+        gl.glDrawArraysInstanced(gl.GL_TRIANGLES, 0, 3, triangle_count)
+
     window.push_handlers(on_draw)
 
     pyglet.app.run()
@@ -71,12 +79,22 @@ def setup_render_buffers(models):
     buffers = {}
 
     vao_square = gl.GLuint()
-    vbo_square = gl.GLuint()
-    vbo_square_instances = gl.GLuint()
+    vao_triangle = gl.GLuint()
 
+    vbo_square = gl.GLuint()
+    vbo_triangle = gl.GLuint()
+
+    vbo_square_instances = gl.GLuint()
+    vbo_triangle_instances = gl.GLuint()
+    
     gl.glGenVertexArrays(1, ct.byref(vao_square))
+    gl.glGenVertexArrays(1, ct.byref(vao_triangle))
+
     gl.glGenBuffers(1, ct.byref(vbo_square))
+    gl.glGenBuffers(1, ct.byref(vbo_triangle))
+
     gl.glGenBuffers(1, ct.byref(vbo_square_instances))
+    gl.glGenBuffers(1, ct.byref(vbo_triangle_instances))
 
     gl.glBindVertexArray(vao_square)
 
@@ -95,7 +113,8 @@ def setup_render_buffers(models):
         0,
         ct.c_void_p(0)
     )
-    
+    gl.glEnableVertexAttribArray(0)
+
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo_square_instances)
     gl.glBufferData(
         gl.GL_ARRAY_BUFFER, 
@@ -117,11 +136,58 @@ def setup_render_buffers(models):
         gl.glVertexAttribDivisor(loc, 1)
 
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
+
+    gl.glBindVertexArray(vao_triangle)
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo_triangle)
+    gl.glBufferData(
+        gl.GL_ARRAY_BUFFER,
+        triangle_mesh.nbytes,
+        triangle_mesh.ctypes.data_as(ct.POINTER(ct.POINTER(ct.c_float))),
+        gl.GL_STATIC_DRAW
+    )
+    gl.glVertexAttribPointer(
+        0,
+        3,
+        gl.GL_FLOAT,
+        gl.GL_FALSE,
+        0,
+        ct.c_void_p(0)
+    )
     gl.glEnableVertexAttribArray(0)
+    
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo_triangle_instances)
+    gl.glBufferData(
+        gl.GL_ARRAY_BUFFER, 
+        models['test-triangles'].nbytes,
+        models['test-triangles'].ctypes.data_as(ct.POINTER(ct.c_float)),
+        gl.GL_STATIC_DRAW
+    )
+    for i in range(4):
+        loc = 1 + i
+        gl.glEnableVertexAttribArray(loc)
+        gl.glVertexAttribPointer(
+            loc,
+            4,
+            gl.GL_FLOAT,
+            gl.GL_FALSE, 
+            64,
+            ct.c_void_p(i * 16)
+        )
+        gl.glVertexAttribDivisor(loc, 1)
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
+
+    gl.glBindVertexArray(0)
 
     buffers['vao-square'] = vao_square
     buffers['vbo-square'] = vbo_square
     buffers['vbo_square_instances'] = vbo_square_instances
+
+    buffers['vao-triangle'] = vao_triangle
+    buffers['vbo-triangle'] = vbo_triangle
+    buffers['vbo_triangle_instances'] = vbo_triangle_instances
+
 
     return buffers
 
@@ -162,30 +228,57 @@ def configure_gl():
 
 def configure_scene():
     models = {
-        'test-squares': []
+        'test-squares': [],
+        'test-triangles': []
     }
+    square_count = configure_test_squares(models)
+    triangle_count = configure_test_triangles(models)
 
+    return models, square_count, triangle_count
+
+def configure_test_squares(models):
     grid_size = 4
-
     sep = 0.2
-
-    model_count = 0
+    square_count = 0
+    squares = []
 
     for i in range(grid_size):
         for j in range(grid_size):
-            posx = i * sep
-            posy = j * sep
-            model = np.eye(4, dtype=np.float32)
-            model[0, 3] = posx
-            model[1, 3] = posy
+            posx = i * sep + sep / 2
+            posy = j * sep + sep / 2
+            square = np.eye(4, dtype=np.float32)
+            square[0, 3] = posx
+            square[1, 3] = posy
 
-            models['test-squares'].append(model.T.flatten())
+            squares.append(square.T.flatten())
 
-            model_count += 1
+            square_count += 1
 
-    models['test-squares'] = np.array(models['test-squares'], dtype=np.float32)
+    models['test-squares'] = np.array(squares, dtype=np.float32)
 
-    return models, model_count
+    return square_count
+
+def configure_test_triangles(models):
+    grid_size = 4
+    sep = 0.2
+    triangle_count = 0
+    triangles = []
+
+    for i in range(grid_size):
+        for j in range(grid_size):
+            posx = -i * sep - sep / 2
+            posy = -j * sep - sep / 2
+            triangle = np.eye(4, dtype=np.float32)
+            triangle[0, 3] = posx
+            triangle[1, 3] = posy
+
+            triangles.append(triangle.T.flatten())
+
+            triangle_count += 1
+
+    models['test-triangles'] = np.array(triangles, dtype=np.float32)
+
+    return triangle_count
 
 def initiate_window():
 
